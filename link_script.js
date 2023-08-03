57e71c02bba2932a5aebc4d84069d5b765e28349
full_stack <pratheeshwaran04@gmail.com>
2023-08-03 16:38:30 +0530

    button with functionality

--- link_script.js
+++ link_script.js
@@ -1,1 +1,18 @@
 // document.write("Hello !! I am a 3rd year student")
+// window.print()
+// window.alert("Don't open this page")
+function first() {
+    document.getElementById("hello").innerHTML = "I am in my first year"
+}
+
+function second() {
+    document.getElementById("hello").innerHTML = "I am in my second year"
+}
+
+function third() {
+    document.getElementById("hello").innerHTML = "I am in my third year"
+}
+
+function final() {
+    document.getElementById("hello").innerHTML = "I am in my final year"
+}
