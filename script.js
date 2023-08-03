2f7944d555de702287f91833d26de098abaf3afb
full_stack <pratheeshwaran04@gmail.com>
2023-08-03 11:55:30 +0530

    added java-script

---
+++ script.js
@@ -0,0 +1,20 @@
+var a = 20
+console.log(a)
+{
+    let a = 54
+    console.log(a)
+}
+var a = 10
+console.log(a)
+
+a = 0
+if (a === 0) {
+    console.log("The number is 0 and adding 1 to make it positive")
+    a +=1
+}
+else if (a > 0) {
+    console.log("The number is positive")
+}
+else {
+    console.log("The number is negative")
+}
