cd36fe4aa44490def0b378207f3eb96f75a7d5c1
full_stack <pratheeshwaran04@gmail.com>
2023-08-03 12:26:48 +0530

    aliter for foreach

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-// var a = 20
-// console.log(a)
-// {
-//     let a = 54
-//     console.log(a)
-// }
-// var a = 10
-// console.log(a)
+// basics
+var a = 20
+console.log(a)
+{
+    let a = 54
+    console.log(a)
+}
+var a = 10
+console.log(a)
 
+// if else
 var a = prompt("Enter a number : ")
 console.log("Number : " + a)
 if (a === 0) {
@@ -20,12 +22,23 @@ else {
     console.log("The number is negative")
 }
 
+// for loop
 for (i =0; i < 10; i++) {
     console.log(i)
 }
 
-var j = 11
+// while loop
+var i = 11
 while (i < 21) {
     console.log(i);
     i++;
+}
+
+// for each
+var array = [21,22]
+array.forEach(element => console.log(element))
+
+//aliter for for each
+for (i=0 ; i < array.length; i++) {
+    console.log(array[i])
 }
